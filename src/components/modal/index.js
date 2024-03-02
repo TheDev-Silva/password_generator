b331ec91d063ccfee3ebaa2e3a6323dd5710a54c
Germano Silva <silvadeveloper2024@gmail.com>
2024-03-02 16:53:48 -0300

    chore: Criando um modal para mostrar senha

---
+++ src/components/modal/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native/types";
+
+export function ModalPassword(){
+
+    return (
+        <View style={style.container}>
+            <Text>Modal</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+   container: {
+
+   }
+
+})
