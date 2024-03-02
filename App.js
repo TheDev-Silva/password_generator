45a458e91ecef5c745c6d0c7edac4ef7e89c8da6
Germano Silva <silvadeveloper2024@gmail.com>
2024-03-02 15:08:37 -0300

    feat: dando forma ao app incluindo logo e slider

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native'
+import Slider, {} from '@react-native-community/slider'
+export default function Home(){
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Image source={require("./src/assets/logo.png")}
+      style={style.logo}
+      />
+      <Text style={style.title}>20 Caracteres</Text>
+      <View style={style.area}>
+       <Slider 
+       style={{height: 50}}
+       maximumValue={6}
+       minimumValue={20}
+       maximumTrackTintColor={"#000"}
+       minimumTrackTintColor={"#00ea3d"}
+       thumbTintColor={"#009933"}
+       />
+      </View>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      textAlign: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f4f4f4'
+    },
+    logo : {
+      marginBottom: 60,
+    },
+    title: {
+      fontSize: 28  
+    },
+    area: {
+      marginTop: 14,
+      marginBottom: 14,
+      width: '80%',
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      padding: 6
+    }
+})
