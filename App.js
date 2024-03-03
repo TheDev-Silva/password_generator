5698dc685cbca9af38be6d9d6f52078dfdf5cc89
Germano Silva <silvadeveloper2024@gmail.com>
2024-03-03 10:20:44 -0300

    feat: add de routes

--- App.js
+++ App.js
@@ -1,135 +1,14 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native'
-import Slider, { } from '@react-native-community/slider'
-import { useEffect, useState } from 'react'
-import { ModalPassword } from './src/components/modal';
+import { NavigationContainer } from '@react-navigation/native'
+import { View, navigation } from 'react-native'
+import { Routes } from './src/routes'
 
 
-export default function Home() {
-
-  const [size, setSize] = useState(10);
-  const [passwordValue, setPasswordValue] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
-
-
-  let init = 'Silva'
-  let finish = '2024'
-  let charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-  function generat_Password() {
-
-    let password = "";
-
-    for (let i = 0, n = charSet.length; i < size; i++) {
-      password += charSet.charAt(Math.floor(Math.random() * n))
-    }
-    setPasswordValue(init+password+finish)
-    setModalVisible(true)
-    //setSize('senha gerada:', size)
-  }
+export default function App() {
 
 
   return (
-    <View style={style.container}>
-      <View style={style.secretKey}>
-        <Image source={require("./src/assets/logo2.png")}
-          style={style.logo}
-        />
-      </View>
-
-
-      <Text style={style.title}>{size} Caracteres</Text>
-      <View style={style.area}>
-        <Slider
-          style={{ height: 50 }}
-          minimumValue={6}
-          maximumValue={20}
-          maximumTrackTintColor={"#000"}
-          minimumTrackTintColor={"#00ea3d"}
-          thumbTintColor={"#009933"}
-          value={size}
-          onValueChange={(value) => setSize(value.toFixed(0))}
-        />
-      </View>
-      <TouchableOpacity style={style.button} onPress={generat_Password}>
-        <Text style={style.buttonText}>Gerar senha</Text>
-      </TouchableOpacity>
-
-
-
-
-      <Modal visible={modalVisible}
-        animationType="fade"
-        transparent={true}>
-
-        <ModalPassword password={passwordValue} handleClose={() => setModalVisible(false)} />
-      </Modal>
-
-
-
-      <View style={style.autor}>
-        <Text style={style.dev}>Desenvolvido por</Text>
-        <Text style={style.textAutor}>@SilvaDev 2024</Text>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Routes/>
+    </NavigationContainer>
   )
 }
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f4f4f4'
-  },
-  
-  autor: {
-    position: 'absolute',
-    bottom: 10,
-    alignItems: 'center'
-  },
-  dev: {
-    textTransform: 'uppercase',
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-  textAutor: {
-    fontSize: 13,
-  },
-  logo: {
-    marginBottom: 20,
-    marginTop: 20,
-  },
-  secretKey: {  
-    flexDirection:'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  },
-  title: {
-    fontSize: 25,
-    fontWeight: 'bold'
-  },
-  area: {
-    marginTop: 12,
-    marginBottom: 12,
-    width: '80%',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 6
-  },
-  button: {
-    borderRadius: 5,
-    backgroundColor: '#EDBB00',
-    width: '80%',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
-
-  },
-  buttonText: {
-    color: "#000",
-    fontSize: 18,
-    fontWeight: "bold",
-    textTransform: 'uppercase'
-
-  }
-})
---
+++ src/pages/password/index.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export function Passwords() {
+    return (
+        <View style={style.container}>
+            <Text>
+                Minhas Senhas
+            </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+
+    }
+})
