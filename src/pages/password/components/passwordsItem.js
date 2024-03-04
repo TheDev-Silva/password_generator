96e4dfb980babb1f9c06b66a1fb5c4906d14f5b1
Germano Silva <silvadeveloper2024@gmail.com>
2024-03-04 10:30:14 -0300

    feat: melhorias

--- src/pages/password/components/passwordsItem.js
+++ src/pages/password/components/passwordsItem.js
@@ -1,16 +1,28 @@
+import { useState } from 'react'
 import { Text, StyleSheet, Pressable, ScrollView } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 export function PasswordsItem({ data, removePassword }) {
 
+    const [passwordVisible, setPasswordVisible] = useState(false)
     const [password, motivo] = data.split('|')
+    const Tab = createBottomTabNavigator()
+
+    const togglePasswordVisibility = () => {
+
+        setPasswordVisible(!passwordVisible)
+    }
 
     return (
         <ScrollView style={style.scrollView}>
             <Pressable
                 onLongPress={removePassword}
+                onPress={togglePasswordVisibility}
                 style={style.container}>
-                <Text style={style.textPassword}>{password}</Text>
-                {motivo && <Text style={style.textMotivo}>Motivo: {motivo}</Text>}
+                <Text style={style.textMotivo}>{motivo}</Text>
+                <Ionicons name={passwordVisible ? 'ios-arrow-up' : 'ios-arrow-down'} size={18} color={'#fff'}/>
+                {passwordVisible && <Text style={style.textPassword}>{password}</Text>}
             </Pressable>
         </ScrollView>
 
@@ -23,20 +35,22 @@ const style = StyleSheet.create({
     },
     container: {
         flex: 1,
+        flexDirection: 'row',
         backgroundColor: "#0e0e0e",
         padding: 14,
         width: "100%",
         marginBottom: 10,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         borderRadius: 8
     },
     textPassword: {
         color: '#fff',
-        fontSize: 18,
+        fontSize: 16,
     },
     textMotivo: {
         color: '#fff',
-        fontSize: 18
+        fontSize: 18,
+        textTransform: 'uppercase'
     }
 })
